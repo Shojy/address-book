31bad81e8d65f58fead05e4042bc10faa3bf461e
Joshua Moon <joshua@moon.uk.net>
2016-01-11 07:50:05 +0000

    Added gravatar support and additional fields to forms.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,52 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @contacts = Contact.all.order('last_name asc, first_name asc')
+  end
+
+  def show
+
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to @contact, notice: 'Contact created successfully.'
+    else
+      render 'new', alert: 'Errors occured while saving'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: 'Contact updated'
+    else
+      render 'edit', alert: 'Errors occurred updating.'
+    end
+  end
+
+  def destroy
+    @contact.destroy
+
+    redirect_to contacts_path, notice: 'Contact successfully deleted.'
+  end
+
+  private
+
+    def find_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email, details_attributes: [:label, :value])
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ActiveRecord::Base
+  has_many :details
+
+
+  accepts_nested_attributes_for :details, reject_if: proc { |attributes| attributes['value'].blank? }, allow_destroy: true
+
+  def gravatar_url
+    hash = Digest::MD5.hexdigest(email.downcase.strip)
+    "https://gravatar.com/avatar/#{hash}.png?d=mm&s=32"
+  end
 end
